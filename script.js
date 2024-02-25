8e1c84f20ff44493d20ff41a7b57a49127cf51fd
Collette <garlandcollette@gmail.com>
2024-02-25 09:55:28 -0330

    google form added

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-document.getElementById("bookingButton").onclick = function() {
-    window.location.href = "book.html"; // Assuming "book.html" is the target page
-  }
-  
+document.getElementById("bookingButton").addEventListener("click", function() {
+  window.location.href = "https://docs.google.com/forms/d/e/1FAIpQLSczINhoEoTdVTu_xHFYQ-z4OJ5KOyhYu8Bu8xT8L1ltFDAyqw/viewform";
+});
